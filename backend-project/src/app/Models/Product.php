ef9e45633ab9e8862b69b288b5a6d0ad3739a5a5
rmerzak <rmerzak@student.1337.ma>
2023-10-18 15:06:01 +0100

    Added Category CategoryProduct and Product Models

---
+++ backend-project/src/app/Models/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'description', 'price', 'image'];
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_products', 'product_id', 'category_id');
+    }
+}
