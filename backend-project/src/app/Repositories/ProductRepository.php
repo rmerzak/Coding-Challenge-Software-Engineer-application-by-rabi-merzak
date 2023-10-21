4251b1e31c06da6c9e471e62270a961d082be644
rmerzak <rmerzak@student.1337.ma>
2023-10-21 15:59:36 +0100

    Update product repo

--- backend-project/src/app/Repositories/ProductRepository.php
+++ backend-project/src/app/Repositories/ProductRepository.php
@@ -39,43 +39,8 @@ class ProductRepository
 
     public function save($data)
     {
-        $product = new $this->product;
-
-        if ($data->hasFile('image')) {
-            $filenameWithExt = $data->file('image')->getClientOriginalName();
-            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-            $extension = $data->file('image')->guessClientExtension();
-            $fileNameToStore = $filename . '_' . time() . '.' . $extension;
-            $path = $data->file('image')->storeAs('public/image', $fileNameToStore);
-        } else {
-            $fileNameToStore = 'no.png';
-        }
-
-        $product->name = $data->input('name');
-        $product->description = $data->input('description');
-        $product->price = $data->input('price');
-        $product->image = $fileNameToStore;
-
-        $product->save();
-
-        return $product->fresh();
-    }
-
-    public function update($data, $id)
-    {
-        $product = $this->product->find($id);
-
-        if (!$product) {
-            throw new InvalidArgumentException('Product not found');
-        }
-
-        // $product->update([
-        //     'name' => $data->input('name'),
-        //     'description' => $data->input('description'),
-        //     'price' => $data->input('price'),
-        // ]);
-
-        return $product;
+        $data->save();
+        return $data->fresh();
     }
 
     public function delete($id)
