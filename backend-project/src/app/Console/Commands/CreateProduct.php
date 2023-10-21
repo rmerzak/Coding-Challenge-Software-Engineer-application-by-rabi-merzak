59fefb42060b3aa3e1952dc069c4e8b22ef4babb
rmerzak <rmerzak@student.1337.ma>
2023-10-21 16:00:31 +0100

    Add categorie in CreateProduct

--- backend-project/src/app/Console/Commands/CreateProduct.php
+++ backend-project/src/app/Console/Commands/CreateProduct.php
@@ -5,35 +5,85 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Services\ProductService;
 use Illuminate\Http\UploadedFile;
+use App\Repositories\CategoryRepository;
 class CreateProduct extends Command
 {
     protected $signature = 'create:product';
     protected $description = 'Create a new product';
-    public function __construct()
+    protected $categoryRepository;
+
+    public function __construct(CategoryRepository $categoryRepository)
     {
         parent::__construct();
+        $this->categoryRepository = $categoryRepository;
     }
+
     public function handle()
     {
-        $data = [
-            'name' => $this->ask('Enter the product name:'),
-            'description' => $this->ask('Enter the product description:'),
-            'price' => $this->ask('Enter the product price:'),
-        ];
+        try {
+            $data = $this->gatherProductData();
+
+            $productService = app(ProductService::class);
+            $product = $productService->saveProductData($data);
+            $categories = $this->categoryRepository->getAll();
+            $categoryIdsInput = $this->askForCategoryIds($categories);
+            $categoryIds = explode(',', $categoryIdsInput);
+            $this->outputResult(200, 'Product created successfully!', $product);
+
+        } catch (\Exception $e) {
+            $this->outputResult(500, 'Product creation failed: ' . $e->getMessage());
+        }
+    }
+
+    protected function gatherProductData()
+    {
+        $name = $this->ask('Enter the product name:');
+        $description = $this->ask('Enter the product description:');
+        $price = $this->ask('Enter the product price:');
         $imagePath = $this->ask('Enter the path to the image file (leave empty if none):');
-        if (!empty($imagePath) && file_exists($imagePath)) {
-            $image = new UploadedFile($imagePath, pathinfo($imagePath, PATHINFO_FILENAME));
-            $path = $image->store('images', 'public');
-            $data['image'] = $path;
-        } else {
-            $data['image'] = 'no.png';
+
+        $data = [
+            'name' => $name,
+            'description' => $description,
+            'price' => $price,
+            'image' => $this->storeImage($imagePath),
+        ];
+
+        return $data;
+    }
+
+    protected function storeImage($imagePath)
+    {
+        if (empty($imagePath) || !file_exists($imagePath)) {
+            return 'no.png';
         }
-        $productService = app(ProductService::class);
-        $result = $productService->saveProductData($data);
-        if ($result['status'] === 200) {
-            $this->info('Product created successfully!');
+
+        $image = new UploadedFile($imagePath, pathinfo($imagePath, PATHINFO_FILENAME));
+        return $image->store('images', 'public');
+    }
+
+    protected function outputResult($status, $message, $product = null)
+    {
+        $result = ['status' => $status];
+
+        if ($status === 200) {
+            $result['product'] = $product;
+            $this->info($message);
         } else {
-            $this->error('Product creation failed: ' . $result['error']);
+            $result['error'] = $message;
+            $this->error($message);
         }
+
+        $this->info(json_encode($result));
+    }
+    protected function askForCategoryIds($categories)
+    {
+        $categoryIds = [];
+        $categoriesList = [];
+        foreach ($categories as $category) {
+            $categoriesList[] = $category->id . ': ' . $category->name;
+        }
+        $categoryIdsInput = $this->ask('Select category IDs from the list below (comma-separated)[X,Y,...]:', implode(', ', $categoriesList));
+        return $categoryIdsInput;
     }
 }
