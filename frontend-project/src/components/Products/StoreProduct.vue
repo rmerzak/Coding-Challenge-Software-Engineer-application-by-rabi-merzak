936286fe950ddb1379b110df99eba1b1a96da754
rmerzak <rmerzak@student.1337.ma>
2023-10-28 20:35:50 +0100

    Update Form Product

--- frontend-project/src/components/Products/StoreProduct.vue
+++ frontend-project/src/components/Products/StoreProduct.vue
@@ -2,37 +2,52 @@
   <div class="bloc-modale" v-if="revele">
     <div class="overlay" v-on:click="toggleModale"></div>
 
-    <div class="modale card">
-      <div v-on:click="toggleModale" class="btn-modale btn btn-danger">X</div>
-      <input type="text" class="form-control" placeholder="Name" v-model="name" />
-      <input type="text" class="form-control" placeholder="Price" v-model="price" />
-      <input type="text" class="form-control" placeholder="Description" v-model="description" />
-      <label for="files" class="btn">Select Image :</label>
-      <input id="file" class="input-file" @change="handleFileChange" type="file" />
-      <div>Select Category :</div>
+    <form @submit.prevent="submit">
+      <div class="modale card">
+        <div v-on:click="toggleModale" class="btn-modale btn btn-danger">X</div>
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Name" v-model="name" />
+        </div>
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Price" v-model="price" />
+        </div>
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Description" v-model="description" />
+        </div>
+        <div class="form-group">
+          <label for="file" class="btn">Select Image :</label>
+          <input id="file" class="input-file" @change="handleFileChange" type="file" />
+        </div>
+        <div>Select Category :</div>
+        <div class="form-group">
       <div class="category-list">
         <label
           v-for="element in Category"
           :key="element.id"
           :class="{ 'selected-category': element.selected }"
         >
-          <input type="checkbox" v-model="element.selected" /> {{ element.name }}
+          <input type="checkbox" v-model="element.selected" /> {{ element.name }} ,
         </label>
       </div>
-      <div v-if="showAlert" class="alert" :class="alertType">{{ errorMessage }}</div>
-      <button class="btn btn-primary" @click="submit">Add Product</button>
-    </div>
+      </div>
+        <div v-if="showAlert" class="alert" :class="alertType">{{ errorMessage }}</div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary">Add Product</button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
-<script>
-import axios from 'axios';
 
+<script>
+import {validateForm, selectedCategorys} from '../../helpers/FormHelpers'
+import CategoryAPIService from "../../services/category/CategoryAPIService";
+import ProductsAPIService from "../../services/products/ProductsAPIService";
+const categoryAPIService = new CategoryAPIService();
+const productsAPIService = new ProductsAPIService();
 export default {
-  name: "Modale",
   props: ["revele", "toggleModale"],
-  components: {
-  },
   data() {
     return {
       name: "",
@@ -43,63 +58,55 @@ export default {
       Category: [],
     };
   },
-  created() {
-    axios.get('http://localhost/category')
-      .then(response => {
-        console.log(response.data.data)
-        this.Category = response.data.data.category;
-      })
-      .catch(error => {
-        console.error('Error fetching data: ' + error);
-      });
+  async created() {
+    const [error, data] = await categoryAPIService.getCategory();
+    if (error) console.error("Error fetching data:", error);
+    else this.Category = data.data.category;
   },
   computed: {
     selectedNames() {
-      return this.Category.filter((element) => element.selected).map((element) => element.id);
+      return selectedCategorys(this.Category);
     },
   },
-methods: {
+  methods: {
     handleFileChange(event) {
       this.selectedImage = event.target.files[0];
     },
-  async submit() {
-      const formData = new FormData();
-      if (!this.name || !this.description || isNaN(parseFloat(this.price))) {
-        this.errorMessage = "Erreur : required fields";
-        console.log(this.errorMessage);
-        return;
-      }
-      formData.append("name", this.name);
-      formData.append("price", this.price);
-      formData.append("description", this.description);
-      console.log(this.selectedNames)
-      if (this.selectedNames) {
-        formData.append("category_ids", this.selectedNames.join(","));
-      }
-
-      if (this.selectedImage) {
-        formData.append("image", this.selectedImage);
-      }
-console.log(formData)
-      axios
-        .post("http://localhost/products", formData)
-        .then((response) => {
-          this.alertMessage = "Product added successfully";
-          this.alertType = "success";
-          this.showAlert = false;
-          this.name = "";
-          this.price = "";
-          this.description = "";
-          this.value = null;
-          this.selectedImage = null;
-          const newProduct = response.data.product;
-            this.$emit("updateProductData", newProduct);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+    clearForm() {
+      this.alertType = "success";
+      this.showAlert = false;
+      this.name = "";
+      this.price = "";
+      this.description = "";
+      this.value = null;
+      this.selectedImage = null;
     },
-},
+    async submit() {
+      if (validateForm(this.name,this.description,this.price)) {
+        const formData = new FormData();
+        formData.append("name", this.name);
+        formData.append("price", this.price);
+        formData.append("description", this.description);
+
+        if (this.selectedNames) {
+          formData.append("category_ids", this.selectedNames.join(","));
+        }
+        if (this.selectedImage) {
+          formData.append("image", this.selectedImage);
+        }
+
+        const [error, data] = await productsAPIService.addProduct(formData);
+        if (error) {
+          console.error("Error Adding data:", error);
+        } else {
+          console.log(data);
+          this.clearForm();
+        }
+      } else {
+        this.errorMessage = "Erreur : required fields";
+      }
+    },
+  },
 };
 </script>
 
@@ -158,10 +165,13 @@ console.log(formData)
   background-color: #0d6efd;
   color: #ffffff;
   border-radius: 6px;
-  padding: 6px;
-  margin: 5px;
+  padding: 4px;
+  margin: 4px;
 }
 .form-control {
   margin: 5px;
+}
+.btn-primary {
+  background-color: #0d6efd;
 }
 </style>
